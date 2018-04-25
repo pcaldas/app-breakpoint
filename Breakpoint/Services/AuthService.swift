327e68387124d2217ce43c4521f61c8c60db8f29
Paulo Caldas <paulocaldas@MacBook-Pro-de-Paulo.local>
2018-04-24 21:16:02 -0300

    create the AuthService view

---
+++ Breakpoint/Services/AuthService.swift
@@ -0,0 +1,36 @@
+//
+//  AuthService.swift
+//  Breakpoint
+//
+//  Created by Paulo Caldas on 24/04/2018.
+//  Copyright © 2018 PMC. All rights reserved.
+//
+
+import Foundation
+import Firebase
+
+class AuthService {
+    static let instance = AuthService()
+    
+    func registerUser(withEmail email: String, andPassword password: String, userCreationComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().createUser(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                userCreationComplete(false, error)
+                return
+            }
+            let userData = ["provider": user.providerID, "email": user.email]
+            DataService.instance.createDBUser(uid: user.uid, userData: userData)
+            userCreationComplete(true, nil)
+        }
+    }
+    
+    func loginUser(withEmail email: String, andPassword password: String, loginComplete: @escaping (_ status: Bool, _ error: Error?) -> ()) {
+        Auth.auth().signIn(withEmail: email, password: password) { (user, error) in
+            guard let user = user else {
+                loginComplete(false, error)
+                return
+            }
+            loginComplete(true, nil)
+        }
+    }
+}
