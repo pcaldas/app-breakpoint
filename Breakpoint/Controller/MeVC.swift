84a2dabffcaf5aa41308a7e9edb1fd92c599a5ac
Paulo Caldas <paulocaldas@MacBook-Pro-de-Paulo.local>
2018-04-25 00:15:57 -0300

    create MeVC

---
+++ Breakpoint/Controller/MeVC.swift
@@ -0,0 +1,28 @@
+//
+//  MeVC.swift
+//  Breakpoint
+//
+//  Created by Paulo Caldas on 25/04/2018.
+//  Copyright © 2018 PMC. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+    
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signOutBtnWasPressed(_ sender: Any) {
+    }
+    
+
+}
