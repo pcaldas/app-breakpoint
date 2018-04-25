84bfddcd0d3e940515719e777ac4eb4dd0cb281d
Paulo Caldas <paulocaldas@MacBook-Pro-de-Paulo.local>
2018-04-25 11:27:54 -0300

    create the msg feed

---
+++ Breakpoint/Model/Message.swift
@@ -0,0 +1,27 @@
+//
+//  Message.swift
+//  Breakpoint
+//
+//  Created by Paulo Caldas on 25/04/2018.
+//  Copyright © 2018 PMC. All rights reserved.
+//
+
+import Foundation
+
+class Message {
+    private var _content: String
+    private var _senderId: String
+    
+    var content: String {
+        return _content
+    }
+    
+    var senderId: String {
+        return _senderId
+    }
+    
+    init(content: String, senderId: String) {
+        self._content = content
+        self._senderId = senderId
+    }
+}
