3f48e8c19bf0a96e9f7a2570ec697c9bcfa99b32
Paulo Caldas <paulocaldas@MacBook-Pro-de-Paulo.local>
2018-04-25 18:47:10 -0300

    add people to the group

--- Breakpoint/View/UserCell.swift
+++ Breakpoint/View/UserCell.swift
@@ -16,6 +16,8 @@ class UserCell: UITableViewCell {
     @IBOutlet weak var emailLbl: UILabel!
     @IBOutlet weak var checkImage: UIImageView!
     
+    var showing = false
+    
     func configureCell(profileImage image: UIImage, email: String, isSelected: Bool) {
         self.profileImage.image = image
         self.emailLbl.text = email
@@ -29,7 +31,16 @@ class UserCell: UITableViewCell {
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        
+        if selected {
+            if showing == false {
+                checkImage.isHidden = false
+                showing = true
+            } else {
+                checkImage.isHidden = true
+                showing = false
+            }
+            
+        }
     }
 
 }
